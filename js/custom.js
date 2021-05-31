2bdce2c2af53638ce0930a602bf1b37ef5e26d3e
Safayethossain95 <safayet.hossain95@gmail.com>
2021-05-31 20:30:35 +0600

    teachers

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$('.teacher-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay:false,
+    prevArrow:"<i class='fa fa-chevron-left left'></i>",
+    nextArrow:"<i class='fa fa-chevron-right right'></i>",
+
+  });
