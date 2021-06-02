79d04f29c4674f0a6edf0e94cce0ed8fa06af367
Safayethossain95 <safayet.hossain95@gmail.com>
2021-06-02 17:03:24 +0600

    s-story

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,24 @@ $('.teacher-slider').slick({
     nextArrow:"<i class='fa fa-chevron-right right'></i>",
 
   });
+
+
+
+  $('.comments-slider-main').slick({
+    
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay:false,
+    prevArrow:"<i class='fa fa-chevron-left left'></i>",
+    nextArrow:"<i class='fa fa-chevron-right right'></i>",
+
+  });
+
+
+  // venubox js start
+  $('.venobox').venobox();  
+  // venubox js end
+  
